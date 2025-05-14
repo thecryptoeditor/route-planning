0858cd8a171fc453ff9d8aa86f388bd9ccfd55e4
jatin1213 <jatin.aggarwal.15csc@bml.edu.in>
2025-05-14 20:25:23 +0530

    GanttChart component created in components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import GanttChart from './components/GanttChart';
+
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <h1>Route Planning Gantt View</h1>
+      <GanttChart />
+    </div>
   );
 }
 
